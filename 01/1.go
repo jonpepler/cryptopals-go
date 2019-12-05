cf878900bd4c4ef2297399e3686feafb692b8706
Jon Pepler <jonathan.pepler@gmail.com>
2019-12-05 17:31:48 +0000

    1-1: Write basic test

---
+++ 01/1.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+}
+
+func hexToBase64(hex string) string {
+	return "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+}
---
+++ 01/1_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestHexToBase64(t *testing.T) {
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	expectedResult := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	if hexToBase64(input) != expectedResult {
+		t.Errorf("Expected '%v', got '%v' :(", input, expectedResult)
+	}
+}
