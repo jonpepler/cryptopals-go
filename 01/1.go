70d05eb2d96d6db285ee9f95ad8da4163c0a47cf
Jon Pepler <jonathan.pepler@gmail.com>
2019-12-05 17:40:18 +0000

    1-1: Decode hex strings to base64

--- 01/1.go
+++ 01/1.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
 func main() {
-	hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+	fmt.Println(hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
 }
 
-func hexToBase64(hex string) string {
-	return "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+func hexToBase64(hexString string) string {
+	bytes, _ := hex.DecodeString(hexString)
+	return base64.StdEncoding.EncodeToString(bytes)
 }
